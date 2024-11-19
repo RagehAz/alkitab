1e931fca53aea21574d7cf686c9f17ce84b82f77
RagehAz <rageh.az@gmail.com>
2024-11-19 11:28:09 +0300

    v1.0 : good

--- lib/c_protocols/providers/alkitab_pro.dart
+++ lib/c_protocols/providers/alkitab_pro.dart
@@ -1,5 +1,9 @@
+import 'package:bldrs/c_protocols/main_providers/general_provider.dart';
 import 'package:bldrs/c_protocols/main_providers/home_pro.dart';
 import 'package:bldrs/c_protocols/main_providers/ui_provider.dart';
+import 'package:bldrs/c_protocols/note_protocols/provider/notes_provider.dart';
+import 'package:bldrs/c_protocols/user_protocols/user/user_pro.dart';
+import 'package:bldrs/c_protocols/zone_protocols/modelling_protocols/provider/zone_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
@@ -20,9 +24,10 @@ class AlKitabProviders extends StatelessWidget {
       providers: <SingleChildWidget>[
         ChangeNotifierProvider<HomePro>(create: (BuildContext ctx) => HomePro()),
         ChangeNotifierProvider<UiPro>(create: (BuildContext ctx) => UiPro()),
-        // ChangeNotifierProvider<UserPro>(create: (BuildContext ctx) => UserPro()),
-        // ChangeNotifierProvider<GeneralPro>(create: (BuildContext ctx) => GeneralPro()),
-        // ChangeNotifierProvider<NotesPro>(create: (BuildContext ctx) => NotesPro()),
+        ChangeNotifierProvider<UserPro>(create: (BuildContext ctx) => UserPro()),
+        ChangeNotifierProvider<GeneralPro>(create: (BuildContext ctx) => GeneralPro()),
+        ChangeNotifierProvider<NotesPro>(create: (BuildContext ctx) => NotesPro()),
+        ChangeNotifierProvider<ZonePro>(create: (BuildContext ctx) => ZonePro()),
         // ChangeNotifierProvider<FcPro>(create: (BuildContext ctx) => FcPro()),
       ],
       child: child,
